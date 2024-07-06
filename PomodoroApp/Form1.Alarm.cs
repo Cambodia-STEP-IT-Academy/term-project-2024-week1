b10f7ef2de3be6d5bb3e70e8bf115a4940d1d2c4
srengty <srengty@gmail.com>
2024-07-06 15:42:08 +0700

    Prepare for task division

---
+++ PomodoroApp/Form1.Alarm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroApp
+{
+    public partial class Form1 : Form
+    {
+        public void Alarm()
+        {
+
+        }
+    }
+}
--- PomodoroApp/Form1.cs
+++ PomodoroApp/Form1.cs
@@ -2,6 +2,8 @@ namespace PomodoroApp
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer timer;
+        private int lapses = 0;
         public Form1()
         {
             InitializeComponent();
@@ -10,6 +12,16 @@ namespace PomodoroApp
         private void ButtonStart_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Timer Started");
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Timer Stopped");
+        }
+
+        private void WorkDuration_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
